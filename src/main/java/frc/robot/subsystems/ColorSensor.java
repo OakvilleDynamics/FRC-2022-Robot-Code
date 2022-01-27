df39e37f96f1529e5420303b798858e839dbc758
Sigma Computer <flamingpuffins@gmail.com>
2022-01-27 14:16:00 -0800

    initial color sensor stuff

--- src/main/java/frc/robot/subsystems/ColorSensor.java
+++ src/main/java/frc/robot/subsystems/ColorSensor.java
@@ -8,23 +8,25 @@ import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.util.Color;
 
-//import  com.revrobotics.ColorSensorV3;
+import  com.revrobotics.ColorSensorV3;
 
 
 public class ColorSensor extends SubsystemBase {
-  /** Creates a new ColorSensor. */
-
-  /*
-  public ColorSensor() {
-    private final I2C.Port i2cPort = I2C.Port.kOnboard;
+  /** Creates a new ColorSensor. */ 
+  final I2C.Port i2cPort = I2C.Port.kOnboard;
     
-    private final ColorSensorV3 m_colorsensor = new ColorSensorV3(i2cPort);
-
-  }
-*/
+  final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    Color detectedColor = m_colorSensor.getColor();
+    int proximity = m_colorSensor.getProximity();
+
+    System.out.print("Red: " + detectedColor.red);
+    System.out.print("Green: " + detectedColor.green);
+    System.out.print("Blue: " + detectedColor.blue);
+    System.out.print("Proximity: " + proximity);
+
   }
 }
