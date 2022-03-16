850937a37b3b3a6bc8c6071fb6205a98abe2f95b
Ooglely <alennavdic@gmail.com>
2022-03-16 17:03:49 -0500

    Climb method

--- src/main/java/frc/robot/commands/ClimbElevator.java
+++ src/main/java/frc/robot/commands/ClimbElevator.java
@@ -39,7 +39,9 @@ public class ClimbElevator extends CommandBase {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_climbingsystem.climb(auxController.getLeftY(), auxController.getRightY());
+  }
 
   // Called once the command ends or is interrupted.
   @Override
