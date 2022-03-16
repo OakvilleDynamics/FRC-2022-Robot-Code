cff2de8d6f68f55ac5e1685426f565571b8421ce
Ooglely <alennavdic@gmail.com>
2022-03-16 17:58:26 -0500

    Barebones Intake system implementation

---
+++ src/main/java/frc/robot/commands/IntakeRamp.java
@@ -0,0 +1,47 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.subsystems.IntakeSystem;
+
+public class IntakeRamp extends CommandBase{
+
+
+    private final IntakeSystem m_intakesystem;
+
+    public IntakeRamp(IntakeSystem subsystem) {
+        m_intakesystem = subsystem;
+        addRequirements(m_intakesystem);
+    }
+
+    private final XboxController auxController = new XboxController(Constants.auxControllerPort);
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {}
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+
+        if (auxController.getRightTriggerAxis() > 0.25) {
+            m_intakesystem.in();
+        }
+
+        if (auxController.getLeftTriggerAxis() > 0.25) {
+            m_intakesystem.out();
+        }
+        
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {}
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+    return false;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/IntakeSystem.java
@@ -0,0 +1,21 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeSystem extends SubsystemBase {
+    
+    public IntakeSystem() {}
+
+    @Override
+    public void periodic() {
+    // This method will be called once per scheduler run
+    }
+
+    public void in() {
+        // Todo
+    }
+
+    public void out() {
+        // Todo
+    }
+}
